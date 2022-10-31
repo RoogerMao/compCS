32ed9d31b029a7d181c9289c32aa1081bede91a5
RoogerMao <112263264+RoogerMao@users.noreply.github.com>
2022-10-31 13:19:41 -0400

    Update tttt.cpp

--- USACO/bronze/tttt.cpp
+++ USACO/bronze/tttt.cpp
@@ -1,46 +1,49 @@
 #include <iostream>
-#include <vector>
 #include <cstdio>
+#include <vector>
 using namespace std;
+typedef long long ll;
+
+pair<int, int> areWins(string line) {
+	if (line[0] == line[1] && line[1] == line[2]) {
+		return pair<int, int>(1, 0);
+	} else if (line[0] == line[1] || line[1] == line[2] || line[0] == line[2]) { 
+		return pair<int, int>(0, 1); 
+	} else { 
+		return pair<int, int>(0, 0); 
+	}
+}
+
+pair<int, int> sumPairs(pair<int, int> p1, pair<int, int> p2) {
+	return pair<int, int>(p1.first + p2.first, p1.second + p2.second);
+}
+
 int main() {
-    freopen("tttt.in", "r", stdin);
-    freopen("tttt.out", "w", stdout);
-    vector<string> rows;
-    int oneCow = 0, twoCows = 0;
-    for(int i = 0; i < 3; i++) {
-        string row; cin >> row;
-        rows.push_back(row);
-    }
-    
-    //rows + columns
-    char one, two, three;
-    for(int i = 0; i < 3; i++) {
-        one = rows[i][0];
-        two = rows[i][1];
-        three = rows[i][2];
-        if(one == two == three) {oneCow;}
-        if(one == two || two == three || one == three) {twoCows++;}
-        
-        one = rows[0][i];
-        two = rows[1][i];
-        three = rows[2][i];
-        if(one == two == three) {oneCow;}
-        if(one == two || two == three || one == three) {twoCows++;}
-    }
-    
-    //diagonals
-    one = rows[0][0]; 
-    two = rows[1][1];
-    three = rows[2][2];
-    if(one == two == three) {oneCow;}
-    if(one == two || two == three || one == three) {twoCows++;}
-    
-    one = rows[2][0];
-    three = rows[0][2];
-    if(one == two == three) {oneCow;}
-    if(one == two || two == three || one == three) {twoCows++;}
-    
-    cout << oneCow << endl << twoCows;
-    
-    return 0;
+	freopen("tttt.in", "r", stdin);
+	freopen("tttt.out", "w", stdout);
+	pair<int, int> wins(0, 0);
+	vector<string> rows;
+	for (int i = 0; i < 3; i++) {
+		string row; cin >> row;
+		rows.push_back(row);
+		wins = sumPairs(wins, areWins(row));
+	}
+	string line = "   ";
+	for (int i = 0; i < 3; i++) {
+		line[0] = rows[0][i];
+		line[1] = rows[1][i];
+		line[2] = rows[2][i];
+		wins = sumPairs(wins, areWins(line));
+	}
+
+	line[0] = rows[2][0];
+	line[1] = rows[1][1];
+	line[2] = rows[0][2];
+	wins = sumPairs(wins, areWins(line));
+
+	line[0] = rows[0][0];
+	line[2] = rows[2][2];
+	wins = sumPairs(wins, areWins(line));
+
+	cout << wins.first << endl << wins.second;
 }
